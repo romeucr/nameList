7a21d2ae356f591d4bde82c2d07321d984ef9f5f
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-12-14 15:15:10 +0100

    Implemented search sorted

--- backend/src/main/java/com/rcrdev/nameList/resources/NameResource.java
+++ backend/src/main/java/com/rcrdev/nameList/resources/NameResource.java
@@ -3,6 +3,7 @@ package com.rcrdev.nameList.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,9 +50,11 @@ public class NameResource {
 	public ResponseEntity<Page<NameDTO>> findName(
 			@RequestParam(value = "name", defaultValue = "") String name,
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "size", defaultValue = "5") Integer size) {
+			@RequestParam(value = "size", defaultValue = "5") Integer size,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
 
-		PageRequest pageRequest = PageRequest.of(page, size);
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy);
 		Page<NameDTO> list = nameService.findName(name, pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
